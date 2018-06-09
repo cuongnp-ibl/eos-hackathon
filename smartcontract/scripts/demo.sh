e236e64df938ee74e53c8698ba5132b1350014e3
Cuong Nguyen <cuongnp@cardano-labo.com>
2018-06-10 05:08:13 +0700

    Merge branch 'feature/api2' into develop

--- smartcontract/scripts/demo.sh
+++ smartcontract/scripts/demo.sh
@@ -7,6 +7,7 @@ nodeos -e -p eosio \
   --wallet-dir . \
   --signature-provider EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV=KEY:5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3 \
   --http-server-address=0.0.0.0:8888 \
+  --filter-on pen:transfer: \
   --filter-on pen:addwhitelist: \
   --filter-on pen:delwhitelist: \
   --filter-on pen:donate: \
